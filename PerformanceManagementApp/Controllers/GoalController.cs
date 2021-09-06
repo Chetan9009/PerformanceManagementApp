b3db910db1d1645ddb9db6aead0e962f12227e57
Chetan <chetanpatil9009@gmail.com>
2021-09-06 13:37:20 +0530

    Day 1

---
+++ PerformanceManagementApp/Controllers/GoalController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PerformanceManagementApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerformanceManagementApp.Controllers
+{
+    public class GoalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(GoalCreateModel goal)
+        {
+            return View();
+        }
+    }
+}
---
+++ PerformanceManagementApp/Models/GoalCreateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerformanceManagementApp.Models
+{
+    public class GoalCreateModel
+    {
+        public int? CreatedBy { get; set; }
+        public string Title { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Score { get; set; }
+    }
+}
